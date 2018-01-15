fb177a2ac958b211e65e8dca01da1ef95668a261
Mark Tran <marktran@Marks-MacBook-Pro.local>
2018-01-15 14:16:11 +0000

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
-console.log("Up and running!");
+var cards = ["queen", "king", "queen", "king"];
+var cardsInPlay = [];
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cardOne;
+cardOne = cards[0];
+cardsInPlay.push(cardOne);
 
 console.log("User flipped " + cardOne);
+
+var cardTwo;
+cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cards[0] === cards [1]){
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+}
+
